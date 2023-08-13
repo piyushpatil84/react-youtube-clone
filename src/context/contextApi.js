5ae8b35ad14ecde297819c0fd8bec75288080599
Piyush Patil <piyush.patil@logixal.com>
2023-08-13 13:03:31 +0530

    first commit youtube clone with header sidebar and feeds

---
+++ src/context/contextApi.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import { getDataFromAPI } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        getDataFromAPI(`search/?q=${query}`).then(({ contents }) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        });
+    };
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory)
+    }, [selectedCategory])
+
+    return (
+        <Context.Provider value={{
+            loading,
+            setLoading,
+            searchResults,
+            setSearchResults,
+            selectedCategory,
+            setSelectedCategory,
+            mobileMenu,
+            setMobileMenu,
+        }}>
+            {props.children}
+        </Context.Provider>
+    )
+}
